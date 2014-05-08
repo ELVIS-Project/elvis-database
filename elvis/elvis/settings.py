755daed8ca1b5fd056ee169eb402dfca049cf1b2
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-05-07 20:58:23 -0400

    Fixing settings.py

--- elvis/elvis/settings.py
+++ elvis/elvis/settings.py
@@ -1,17 +1,8 @@
 """
 Django settings for elvis project.
 
-<<<<<<< HEAD
-SOLR_SERVER = "http://localhost:8983/solr"
-
-# Path to fixture. Used for testing 
-# FIXTURE_DIRS = (
-#    'fixtures/initial_data.json',
-# )
-=======
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
->>>>>>> refs/heads/andrew-changes
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
@@ -74,53 +65,15 @@ WSGI_APPLICATION = 'elvis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-<<<<<<< HEAD
-# Sending emails
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'elvismusicsite@gmail.com'
-EMAIL_HOST_PASSWORD = 'elvismusic'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-LOGIN_URL = "/login/"
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-=======
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'elvis',                      # Or path to database file if using sqlite3.
 
-        'USER': 'ahankins',
+        'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
->>>>>>> refs/heads/andrew-changes
     }
 }
 
@@ -143,4 +96,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-SOLR_SERVER = "http://eowyn.local:8080/elvis-solr"
+SOLR_SERVER = "http://localhost:8080/elvis-solr"
