d3ea57ebac859937d5d8f77f9ef340e2bed611de
Lillio Mok <lillio.mok@mail.mcgill.ca>
2014-06-20 14:03:37 -0400

    Added: serialized url for attachments
    Working towards #22; now have to see if this works consistently on development server.

--- elvis/elvis/serializers/attachment.py
+++ elvis/elvis/serializers/attachment.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 from elvis.models.attachment import Attachment
 
+from django.http import HttpResponse
+from django.core.servers.basehttp import FileWrapper
+import mimetypes
+
+import os
+from django.conf import settings
 
 class AttachmentSerializer(serializers.HyperlinkedModelSerializer):
+    attachment = serializers.SerializerMethodField("retrieve_attachment")
     file_name = serializers.Field()
+    attachment_path = serializers.Field()
+    
     class Meta:
         model = Attachment
-        fields = ("file_name", "id", "url", "old_id", "created", "updated", "description", "uploader")
+        fields = ("file_name", "id", "url", "old_id", "created", "updated", "description", "uploader", "attachment")
+
+    def retrieve_attachment(self, obj):
+        request = self.context.get('request', None)
+        path = os.path.relpath(obj.attachment.path, settings.MEDIA_ROOT)
+        url = request.build_absolute_uri(os.path.join(settings.MEDIA_URL, path))
+        return url
