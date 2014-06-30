38ab79a5dd51a1bd33eadf8dc2d3ac600ecc430b
Lillio Mok <lillio.mok@mail.mcgill.ca>
2014-06-30 15:25:01 -0400

    Added: More generic search parsing
    Logical operators should now work with the new solr template; the old solr template is buggy with this (probably because it uses a default qop).
    
    Some parts of template updated, including a facets column

--- elvis/elvis/helpers/solrsearch.py
+++ elvis/elvis/helpers/solrsearch.py
@@ -32,9 +32,9 @@ class SolrSearch(object):
         self._parse_request()
         self._prepare_query()
         # LM: Debugging prints
-        #print('parsed_request', self.parsed_request, type(self.parsed_request))
-        #print('prepared_query', self.prepared_query, type(self.prepared_query))
-        #print('solr_params', self.solr_params, type(self.solr_params))
+        print('parsed_request', self.parsed_request, type(self.parsed_request))
+        print('prepared_query', self.prepared_query, type(self.prepared_query))
+        print('solr_params', self.solr_params, type(self.solr_params))
 
     def search(self, **kwargs):
         self.solr_params.update(kwargs)
@@ -79,6 +79,7 @@ class SolrSearch(object):
         voice_filt_query = ""
         from_date = "" 
         to_date = ""
+        print qdict.lists()
         for k, v in qdict.lists():
 
             # LM: modified from just self.parsed_request[k] = v to cut out nonsensical page requests to solr
@@ -87,12 +88,17 @@ class SolrSearch(object):
 
             # LM: elif for Type filtration
             # check if user has ticked a filter for querying
-            elif k in settings.SEARCH_FILTERS_DICT:
+            #elif k in settings.SEARCH_FILTERS_DICT:
                 # LM: Update filter_query[], the query string, accordingly
-                if not filter_query:
-                    filter_query += u"{0}{1}".format("type:", settings.SEARCH_FILTERS_DICT[k])
-                else:
-                    filter_query += u"{0}{1}".format(" OR type:", settings.SEARCH_FILTERS_DICT[k])
+            #    if not filter_query:
+            #        filter_query += u"{0}{1}".format("type:", settings.SEARCH_FILTERS_DICT[k])
+            #    else:
+            #        filter_query += u"{0}{1}".format(" OR type:", settings.SEARCH_FILTERS_DICT[k])
+
+            # LM: Elif for Type filtration
+            elif k == 'typefilt':
+                print (k, v)
+                filter_query = "type: ("  + string.join((v), ' OR ') + ") "
 
             # LM: elif for sorting
             elif k == 'sortby':
@@ -100,10 +106,7 @@ class SolrSearch(object):
 
             # LM: elif for Name filtration
             elif k == 'namefilt':
-                    if qdict.get('namefilt') == "":
-                        pass
-                    else:
-                        name_filt_query = "name_general: " + string.join(string.split(v[0]), ' AND name_general: ') 
+                name_filt_query = "name_general: (" + string.join(v) +") "
 
             # LM: elif for Date filtration
             elif k == 'datefiltf':
@@ -111,29 +114,23 @@ class SolrSearch(object):
                     pass
                 elif qdict.get('datefiltf') == "":
                     from_date = " date_general: [ * TO "
-                    to_date = u"{0}-00-00T00:00:00Z ]".format(qdict.get('datefiltt'))
+                    to_date = u"{0}-12-31T23:59:59Z ]".format(qdict.get('datefiltt'))
                 elif qdict.get('datefiltt') == "":
                     from_date =  u" date_general: [ {0}-00-00T00:00:00Z TO ".format(qdict.get('datefiltf'))
                     to_date = "* ]"
                 else:
                     from_date = u" date_general: [ {0}-00-00T00:00:00Z TO ".format(qdict.get('datefiltf'))
-                    to_date = u"{0}-00-00T00:00:00Z ]".format(qdict.get('datefiltt'))
+                    to_date = u"{0}-12-31T23:59:59Z ]".format(qdict.get('datefiltt'))
                 date_filt_query = from_date + to_date
 
             # LM: elif for Tag filtration
             elif k == 'tagfilt':
-                for tag in v[0].split():
-                    if tag_filt_query == "":
-                        tag_filt_query += "tags: " + tag
-                    else:
-                        tag_filt_query += " AND tags: " + tag
+                tag_filt_query = "tags_searchable: (" + string.join(v) + ") "
+
 
             # LM: elif for Voice filtration
             elif k == 'voicefilt':
-                    if qdict.get('voicefilt') == "":
-                        pass
-                    else:
-                        voice_filt_query = "number_of_voices: " +  v[0]
+                voice_filt_query = "number_of_voices: (" + string.join(v) + ") "
 
                 
             elif k == 'rows':
@@ -144,8 +141,7 @@ class SolrSearch(object):
                 if qdict.get(k) == "":
                     v = "*"
                 self.parsed_request[k] = v
-            #elif k == 'format':
-            #    self.parsed_request[k] = v
+
 
         self.solr_params.update({'sort': sort_query})
 
