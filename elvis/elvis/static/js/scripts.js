e8e9ab64eef29671c1e4be9b3e55fb8de142fdac
Lillio Mok <lillio.mok@mail.mcgill.ca>
2014-07-22 16:22:39 -0400

    Added: Checkbox Facets
    Facets are now checkboxes and labels. Jquery function modified using the code for the Duchemin project. Static parseParams script has been changed to allow for multiple facets of the same name but different values.
    
    Fixes #26.

--- elvis/elvis/static/js/scripts.js
+++ elvis/elvis/static/js/scripts.js
@@ -1,24 +1,31 @@
-/**
- * $.parseParams - parse query string paramaters into an object.
- */
-(function($)
-{
+//https://gist.github.com/kares/956897
+
+(function($) {
     var re = /([^&=]+)=?([^&]*)/g;
-    var decodeRE = /\+/g;  // Regex for replacing addition symbol with a space
-    var decode = function (str) {return decodeURIComponent( str.replace(decodeRE, " ") );};
-    $.parseParams = function(query)
-    {
+    var decode = function(str) {
+        return decodeURIComponent(str.replace(/\+/g, ' '));
+    };
+    $.parseParams = function(query) {
         var params = {}, e;
-        while ( e = re.exec(query) )
-        { 
-            var k = decode( e[1] ), v = decode( e[2] );
-            if (k.substring(k.length - 2) === '[]')
-            {
-                k = k.substring(0, k.length - 2);
-                (params[k] || (params[k] = [])).push(v);
+        if (query) {
+            if (query.substr(0, 1) == '?') {
+                query = query.substr(1);
             }
-            else params[k] = v;
+
+            while (e = re.exec(query)) {
+                var k = decode(e[1]);
+                var v = decode(e[2]);
+                if (params[k] !== undefined) {
+                    if (!$.isArray(params[k])) {
+                        params[k] = [params[k]];
+                    }
+                    params[k].push(v);
+                } else {
+                    params[k] = v;
+                }
+            }
         }
         return params;
     };
-})(jQuery);
+})(jQuery); 
+
