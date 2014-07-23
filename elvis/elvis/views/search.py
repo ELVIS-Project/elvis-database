3437c382855d1a6ea19a19f9581f1eaf6642db86
Lillio Mok <lillio.mok@mail.mcgill.ca>
2014-07-23 11:31:29 -0400

    Fixed: Facet Ordering
    After searching through the documentation and trying many different things, I think the only way of getting the facet fields sorted consistently is to have the facets in a list of tuples and not a dict. The sorting is quite hacky and definitely slows down the query process, but I don't see any other way of doing it.
    
    Fixes #11

--- elvis/elvis/views/search.py
+++ elvis/elvis/views/search.py
@@ -14,6 +14,8 @@ from django.http import QueryDict
 
 from django.conf import settings
 
+import operator
+
 class SearchViewHTMLRenderer(CustomHTMLRenderer):
     template_name = "search/search.html"
 
@@ -29,16 +31,35 @@ class SearchView(APIView):
         querydict = request.GET
         #querydict2 = request.GET.getlist('filters') #original 
         #print('querydict', querydict2, type(querydict))
+
         # LM: Some explanations as to what is going on here
         # Constructs a SolrSearch object using the search request; specifically, it parses and prepares the query
         # and assigns it to s
-        #print('request', request)
-        
-        
         # note: Filters, sorts, and other modifiers to solr search are handled in the helper script solrsearch.py
         s = SolrSearch(request) 
 
-        facets = s.facets(['type', 'composer_name', 'tags', 'parent_corpus_name', 'number_of_voices'])  # LM TODO add here when facets are decided
+        facets = s.facets(facet_fields=['type', 'composer_name', 'tags', 'parent_corpus_name', 'number_of_voices'])  # LM TODO add here when facets are decided
+
+        facet_fields = facets.facet_counts['facet_fields']
+        #, key=operator.itemgetter(1)
+        #, key=facet_fields['type'].get, reverse=True
+        #facet_type = {item: facet_fields['type'][item] for item in sorted(facet_fields['type'], key=facet_fields['type'].get, reverse=True)}
+        
+        facet_type = sorted(facet_fields['type'].iteritems(), key=lambda (k,v): (v,k), reverse=True)
+        facet_composer_name = sorted(facet_fields['composer_name'].iteritems(), key=lambda (k,v): (v,k), reverse=True)
+        facet_tags = sorted(facet_fields['tags'].iteritems(), key=lambda (k,v): (v,k), reverse=True)
+        facet_parent_corpus_name = sorted(facet_fields['parent_corpus_name'].iteritems(), key=lambda (k,v): (v,k), reverse=True)
+        facet_number_of_voices = sorted(facet_fields['number_of_voices'].iteritems(), key=lambda (k,v): (v,k), reverse=True)
+
+        facet_fields = {
+            'type': facet_type,
+            'composer_name': facet_composer_name,
+            'tags': facet_tags,
+            'parent_corpus_name': facet_parent_corpus_name,
+            'number_of_voices': facet_number_of_voices,
+        }
+        facets.facet_counts['facet_fields'] = facet_fields
+        print facets.facet_counts
 
         # if we don't have a query parameter, send empty search results
         # back to the template, but still send along the facets.
@@ -97,14 +118,8 @@ class SearchView(APIView):
 
         query_minus_page = query_minus_page.urlencode(['*'])     
 
-        #print(query_minus_page, type(query_minus_page))
-
-
         result = {'results': paged_results, 'facets': facets.facet_counts, 'current_query': query_minus_page, 'FACET_NAMES': settings.FACET_NAMES, 'TYPE_NAMES': settings.TYPE_NAMES}
-        #print('paged_results', paged_results, type(paged_results))
-        #print('result', result)
-        print(result.get('FACET_NAMES'))
-
+        
         # LM: Now cast to REST framework's Response, which is simply the result with the status added to it
         response = Response(result, status=status.HTTP_200_OK)
 
