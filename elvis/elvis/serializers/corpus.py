b59d9b8c02ad5dbcb6d31036cb6c9bd1d3a21f47
Lillio Mok <lillio.mok@mail.mcgill.ca>
2014-08-01 16:58:33 -0400

    Updated: More Templates
    Just adding some more changes before I take a 2 week leave.

--- elvis/elvis/serializers/corpus.py
+++ elvis/elvis/serializers/corpus.py
@@ -15,25 +15,30 @@ class ComposerCorpusSerializer(serializers.HyperlinkedModelSerializer):
 
 class PieceCorpusSerializer(serializers.HyperlinkedModelSerializer):
     composer = ComposerCorpusSerializer()
+    item_id = serializers.Field('pk')
     number_of_movements = serializers.Field(source="number_of_movements")
     class Meta:
         model = Piece
-        fields = ("url", "title", "composer", "number_of_movements")
+        fields = ("url", "title", "composer", "number_of_movements", "date_of_composition", "item_id")
 
+class PieceMovementCorpusSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Piece
+        fields = ("url", "title")
 
 class MovementCorpusSerializer(serializers.HyperlinkedModelSerializer):
     composer = ComposerCorpusSerializer()
+    piece = PieceMovementCorpusSerializer()
+    item_id = serializers.Field('pk')
     class Meta:
         model = Movement
-        fields = ("url", "title", "composer", "piece")
-
+        fields = ("url", "title", "composer", "date_of_composition", "item_id", "piece")
 
 class UserCorpusSerializer(serializers.HyperlinkedModelSerializer):
     full_name = serializers.SerializerMethodField("get_full_name")
     class Meta:
         model = User
         fields = ("url", "full_name")
-
 
     def get_full_name(self, obj):
         if not obj.last_name:
