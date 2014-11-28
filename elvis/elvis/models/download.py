7cd9a5aeb6f68fa356e1d742dd94a55ed367905b
Lillio Mok <lillio.mok@mail.mcgill.ca>
2014-11-28 15:36:40 -0500

    Fixed: Automatic creation of downloads object
    Now listens to User model to instantiate a corresponding Download model.

--- elvis/elvis/models/download.py
+++ elvis/elvis/models/download.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Download(models.Model):
     class Meta:
@@ -11,3 +12,10 @@ class Download(models.Model):
 
     def __unicode__(self):
         return u"{0}".format(self.attachments.all())
+
+def create_user_download(sender, instance, created, **kwargs):
+    if created:
+        Download.objects.create(user=instance)
+
+post_save.connect(create_user_download, sender=User)
+
