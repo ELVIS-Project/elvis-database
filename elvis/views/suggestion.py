9f14e07db7f62d83058b33cb8421b9038e146882
Alex Parmentier <a.g.parmentier@gmail.com>
2015-05-19 10:38:30 -0400

    New: Added piece_title dictionary for autocompletion
    Now the title field will also offer autocomplete. The javascript for auto completion has been couched in a function and the /suggest/ page has been updated to allow multiple dictionaries.

--- elvis/views/suggestion.py
+++ elvis/views/suggestion.py
@@ -9,15 +9,16 @@ import solr
 def suggest(request):
     results = []
 
-    if request.method == "GET" and request.GET.has_key(u'q'):
+    if request.method == "GET" and request.GET.has_key(u'q') and request.GET.has_key(u'd'):
         value = request.GET[u'q']
+        dictionary = request.GET[u'd']
         if len(value) > 1:
-            URLVal = value.replace(" ", "+")
-            json_string = urllib2.urlopen(settings.SOLR_SERVER + "/suggest/?wt=json&q={0}".format(URLVal))
-            data = json.loads(json_string.read())['suggest']['mySuggester']['{0}'.format(value)]
+                URLVal = value.replace(" ", "+")
+                json_string = urllib2.urlopen(settings.SOLR_SERVER + "/suggest/?wt=json&suggest.dictionary={0}&q={1}".format(dictionary, URLVal))
+                data = json.loads(json_string.read())['suggest']['{0}'.format(dictionary)]['{0}'.format(value)]
 
-            if data['numFound'] > 0:
-                for suggestion in data['suggestions']:
-                    results.append({'name': suggestion['term']})
+                if data['numFound'] > 0:
+                    for suggestion in data['suggestions']:
+                        results.append({'name': suggestion['term']})
     j_results = json.dumps(results)
     return HttpResponse(j_results, content_type="json")
