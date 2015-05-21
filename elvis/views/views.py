448334b63ecd76434d9abbc8518e813360ac2bcd
Alex Parmentier <a.g.parmentier@gmail.com>
2015-05-21 16:40:42 -0400

    New: Created views.py file for non-model views
    Currently we have a view/forms.py file which holds a number of half-compelted views for which perform general/utility functions and do not directly correspond to a model. Since these forms are only half working (and not referenced anywhere I can see), I've made a file with a similar function, with a better name (I think). I've moved the solr_suggest method into this file.

--- elvis/views/views.py
+++ elvis/views/views.py
@@ -1,1 +1,24 @@
-__author__ = 'AlexPar'
+from django.http import HttpResponse
+from django.conf import settings
+import json
+import urllib2
+import os
+import zipfile
+import pdb
+
+def solr_suggest(request):
+    results = []
+
+    if request.method == "GET" and request.GET.has_key(u'q') and request.GET.has_key(u'd'):
+        value = request.GET[u'q']
+        dictionary = request.GET[u'd']
+        if len(value) > 1:
+            URLVal = value.replace(" ", "+")
+            json_string = urllib2.urlopen(settings.SOLR_SERVER + "/suggest/?wt=json&suggest.dictionary={0}&q={1}".format(dictionary, URLVal))
+            data = json.loads(json_string.read())['suggest']['{0}'.format(dictionary)]['{0}'.format(value)]
+
+            if data['numFound'] > 0:
+                for suggestion in data['suggestions']:
+                    results.append({'name': suggestion['term']})
+    j_results = json.dumps(results)
+    return HttpResponse(j_results, content_type="json")
