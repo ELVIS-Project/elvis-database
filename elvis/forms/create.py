cfe1fb7211ec66e4a9a3f8d90d50861eb4a7e8a7
Alex Parmentier <a.g.parmentier@gmail.com>
2015-05-29 10:49:27 -0400

    WIP: Simplified creation form for development.

--- elvis/forms/create.py
+++ elvis/forms/create.py
@@ -7,24 +7,27 @@ class PieceForm(forms.Form):
     composer = forms.CharField()
 
     # New composer fields
-    is_new_composer = forms.CharField()
     composer_birth_date = forms.DateField(required=False)
     composer_death_date = forms.DateField(required=False)
 
     collection = forms.CharField(required=False)
-    comp_start = forms.CharField(required=False)
-    comp_end = forms.CharField(required=False)
+
+    composition_start_date = forms.DateField(required=False)
+    composition_end_date = forms.DateField(required=False)
     number_of_voices = forms.IntegerField(required=False)
-    # Additional Fields
-    genre = forms.CharField(required=False)
-    voices = forms.CharField(required=False)
-    composer_country = forms.CharField(required=False)
     language = forms.CharField(required=False)
-    source = forms.CharField(required=False)
+
+
+#    Additional Fields
+    #genre = forms.CharField(required=False)
+    #voices = forms.CharField(required=False)
+    #composer_country = forms.CharField(required=False)
+    #source = forms.CharField(required=False)
+
     # Tags - will probably be complicated.
-    tags = forms.CharField(required=False)
+    #tags = forms.CharField(required=False)
     # Comment
-    comment = forms.CharField(required=False)
+    #comment = forms.CharField(required=False)
 
     # Movements
-    movements = forms.CharField(required=False)
+    #movements = forms.CharField(required=False)
