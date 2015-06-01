912bfdc72fdf184a7672805d6fe294357773256d
Alex Parmentier <a.g.parmentier@gmail.com>
2015-06-01 16:43:35 -0400

    WIP: Added new fields to create form.
    The create form now checks all the fields that are currently in the template.

--- elvis/forms/create.py
+++ elvis/forms/create.py
@@ -5,24 +5,24 @@ class PieceForm(forms.Form):
     # Basic Fields
     title = forms.CharField()
     composer = forms.CharField()
+    collections = forms.CharField(required=False)
+    composition_start_date = forms.DateField(required=False)
+    composition_end_date = forms.DateField(required=False)
 
     # New composer fields
     composer_birth_date = forms.DateField(required=False)
     composer_death_date = forms.DateField(required=False)
 
-    collection = forms.CharField(required=False)
-
-    composition_start_date = forms.DateField(required=False)
-    composition_end_date = forms.DateField(required=False)
     number_of_voices = forms.IntegerField(required=False)
-    language = forms.CharField(required=False)
+    languages = forms.CharField(required=False)
+    genres = forms.CharField(required=False)
+    locations = forms.CharField(required=False)
+    sources = forms.CharField(required=False)
+    instrumentation = forms.CharField(required=False)
 
 
-#    Additional Fields
-    #genre = forms.CharField(required=False)
-    #voices = forms.CharField(required=False)
-    #composer_country = forms.CharField(required=False)
-    #source = forms.CharField(required=False)
+
+
 
     # Tags - will probably be complicated.
     #tags = forms.CharField(required=False)
