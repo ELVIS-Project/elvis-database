699494e0fb8f19c047c73207ff201a9144bcfb8d
Alex Parmentier <a.g.parmentier@gmail.com>
2015-07-22 15:00:37 -0400

    Fixed: Removed more nonworking templates.

--- elvis/elvis/urls.py
+++ elvis/elvis/urls.py
@@ -46,8 +46,8 @@ urlpatterns += format_suffix_patterns(
 
 
         #Password resettting won't work until server emails are up.
-        url(r'^password/reset/$', auth_views.password_reset, {'template_name': 'user/password_reset.html'}, name='password_reset'),
-        url(r'^password/reset/done/$', auth_views.password_reset_done, {'template_name': 'user/password_reset_done.html'}, name='password_reset_done'),
+        url(r'^password/reset/$', auth_views.password_reset, name='password_reset'),
+        url(r'^password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
         url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
         url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
