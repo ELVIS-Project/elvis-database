844df59b8a628dd3a1b76e2e405294df42576f80
Alex Parmentier <a.g.parmentier@gmail.com>
2015-07-29 09:25:46 -0400

    Fixed: Modified donwload badge tooltip.
    Now it says 'Add to Cart' rather than "Add to Collection" on all download badges.
    
    Fixes #62

--- elvis/static/js/elvis-scripts/downloadBadge.js
+++ elvis/static/js/elvis-scripts/downloadBadge.js
@@ -3,7 +3,7 @@ function create_download_badge(item_type, item_id, DOM_id)
 {
     var $DOM = $("#" + DOM_id);
     $DOM.append("<form class='recursive-patch-download-form' action='/downloads/' method='post'>" +
-        "<button type='button' class='btn btn-mini btn-success' data-container='body' data-toggle='tooltip' data-placement='top' title='Add to collection.' ><span class='glyphicon glyphicon-plus'> </span> </button>" +
+        "<button type='button' class='btn btn-mini btn-success' data-container='body' data-toggle='tooltip' data-placement='top' title='Add to Cart.' ><span class='glyphicon glyphicon-plus'> </span> </button>" +
         "<input type='hidden' name='item_type'  value='" + item_type + "'/>" +
         "<input type='hidden' name='item_id'  value='"+ item_id + "'/>" +
         "</form>");
