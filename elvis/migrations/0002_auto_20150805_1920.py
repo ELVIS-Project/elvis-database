8eefef88082ab03b91b4f243755505adda34995f
Alex Parmentier <a.g.parmentier@gmail.com>
2015-08-05 15:21:35 -0400

    New: Proper migration files.
    I was doing these seperatly because I was on different branches, now hopefully they can stay the same.

---
+++ elvis/migrations/0002_auto_20150805_1920.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('elvis', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='download',
+            name='collection_collections',
+            field=models.ManyToManyField(to='elvis.Collection', blank=True),
+        ),
+        migrations.AddField(
+            model_name='download',
+            name='collection_composers',
+            field=models.ManyToManyField(to='elvis.Composer', blank=True),
+        ),
+    ]
