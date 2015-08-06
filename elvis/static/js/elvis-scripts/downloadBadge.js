47092b003b2f126bdd219c9d7a060a704c07ac06
Alex Parmentier <a.g.parmentier@gmail.com>
2015-08-06 16:14:31 -0400

    New: Added function to draw only a download form.
    This will be usefull for drawing buttons based on their 'in_cart' value when doing an ajax GET.

--- elvis/static/js/elvis-scripts/downloadBadge.js
+++ elvis/static/js/elvis-scripts/downloadBadge.js
@@ -6,7 +6,19 @@ function create_download_badge(item_type, item_id, DOM_id)
         "<a class='button btn btn-mini btn-success cart-badge' data-container='body' data-toggle='tooltip' data-placement='top' title='Add to Downloads' ><span class='glyphicon glyphicon-plus'></span></a>" +
         "<input type='hidden' name='item_type'  value='" + item_type + "'/>" +
         "<input type='hidden' name='item_id'  value='" + item_id + "'/>" +
-        "<input type='hidden' name='add' value='add' />" +
+        "<input type='hidden' name='action' value='add' />" +
 
         "</form>");
 }
+
+function create_download_form(item_type, item_id, DOM_id)
+{
+    var $DOM = $("#" + DOM_id);
+    $DOM.append("<form class='recursive-patch-download-form' action='/downloads/' method='post'>" +
+        "<input type='hidden' name='item_type'  value='" + item_type + "'/>" +
+        "<input type='hidden' name='item_id'  value='" + item_id + "'/>" +
+        "<input type='hidden' name='action' value='add' />" +
+
+        "</form>");
+}
+
