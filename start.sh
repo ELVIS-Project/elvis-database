9f5c1c8d051e97e2fae6b9fcff698d3ca73d11a9
Alex Parmentier <a.g.parmentier@gmail.com>
2015-08-07 09:13:40 -0400

    New: Start.sh can be called from anywhere now!
    This neat line changes the directory to the script files directory. Nifty.

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR
+source elvis_virtualenv/bin/activate
 # Opens three tabs in a new terminal window and gets all the processes running for the local server.
 gnome-terminal --tab -e "scripts/start_solr.sh" --tab -e "scripts/start_django.sh" --tab -e "scripts/start_celery.sh"
