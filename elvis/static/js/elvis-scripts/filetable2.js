e6ad075200d8b2cfb6a7f67e745bc1133d916fa5
Alex Parmentier <a.g.parmentier@gmail.com>
2015-08-12 20:20:00 -0400

    WIP: Improvements to new dynamic table.
    Made changes to show more obvious differences between existing items and newly entered items... With all existing things having a different ID pattern, adding new things can use the same handleDynamicTable function that the creation-form uses to create new movements and attachments.

--- elvis/static/js/elvis-scripts/filetable2.js
+++ elvis/static/js/elvis-scripts/filetable2.js
@@ -45,7 +45,7 @@
                 settings.$table.append("<tr id='" + e + settings.table_name + settings.row_count + "'>" +
                     "<td class='text-center'><button id='del_" + settings.table_name + settings.row_count + "' type='button' tabindex='-1' class='btn btn-default'><span class='glyphicon glyphicon-remove'></span></button></td>" +
                     "<td class='text-center' style='padding-top:14px'>" + settings.t_size + "</td>" +
-                    "<td><input name='" + settings.table_name + "_title_" + settings.row_count + "' id='" + settings.table_name + "_title_" + settings.row_count + "' class='form-control' autocomplete='off'" +
+                    "<td><input name='" +  e + settings.table_name + "_title_" + settings.row_count + "' id='" +  e + settings.table_name + "_title_" + settings.row_count + "' class='form-control' autocomplete='off'" +
                     "data-toggle='popover' data-placement='top' data-trigger='focus' data-html='true' title='<b>Movement Title</b>'" +
                     "data-content='If no title is provided, this movement will be ignored on upload. It is not necessary to number movements, as they will retain the ordering in which they are here presented.'> </td>" +
                     "<td><button id='show_advanced_" + settings.table_name + settings.row_count + "' type='button' tabindex='-1'class='btn btn-default'>Show Advanced</button></td></tr>" +
@@ -54,20 +54,20 @@
                     "<div class='row' style='padding-left:12%; padding-right:15px'>" +
                     "<div class='col-md-7'>" +
                     "<div class='form-group'>" +
-                    "<div class='input-group'><span class='input-group-addon'>Instrumentation</span><input name='" + settings.table_name + settings.row_count + "_instrumentation' id='" + settings.table_name + settings.row_count + "_instrumentation' type='text' class='form-control unstyled'></div>" +
-                    "<div id='" + settings.table_name + settings.row_count + "_instrumentation_suggestions' style='padding-left:120px'></div>" +
+                    "<div class='input-group'><span class='input-group-addon'>Instrumentation</span><input name='" +  e + settings.table_name + settings.row_count + "_instrumentation' id='" +  e + settings.table_name + settings.row_count + "_instrumentation' type='text' class='form-control unstyled'></div>" +
+                    "<div id='" +  e + settings.table_name + settings.row_count + "_instrumentation_suggestions' style='padding-left:120px'></div>" +
                     "</div>" +
                     "</div>" +
                     "<div class='col-md-5'>" +
-                    "<div class='btn-group btn-group-justified form-inline radio' style='margin-top: 0px' id='" + settings.table_name + settings.row_count + "_vocalization' data-toggle='buttons'>" +
+                    "<div class='btn-group btn-group-justified form-inline radio' style='margin-top: 0px' id='" +  e + settings.table_name + settings.row_count + "_vocalization' data-toggle='buttons'>" +
                     "<label class='btn btn-default'>" +
-                    "<input type='radio' name='" + settings.table_name + settings.row_count + "_vocalization' value='Vocal'>Vocal" +
+                    "<input type='radio' name='" +  e + settings.table_name + settings.row_count + "_vocalization' value='Vocal'>Vocal" +
                     "</label>" +
                     "<label class='btn btn-default'>" +
-                    "<input type='radio' name='" + settings.table_name + settings.row_count + "_vocalization' value='Instrumental'>Instrumental" +
+                    "<input type='radio' name='" +  e + settings.table_name + settings.row_count + "_vocalization' value='Instrumental'>Instrumental" +
                     "</label>" +
                     "<label class='btn btn-default'>" +
-                    "<input type='radio' name='" + settings.table_name + settings.row_count + "_vocalization' value='Mixed'>Mixed" +
+                    "<input type='radio' name='" +  e + settings.table_name + settings.row_count + "_vocalization' value='Mixed'>Mixed" +
                     "</label>" +
                     "</div>" +
                     "</div>" +
@@ -75,19 +75,19 @@
                     "<div class='row' style='padding-left:12%; padding-right:15px'>" +
                     "<div class='col-md-8'>" +
                     "<div class='form-group'>" +
-                    "<div class='input-group'><span class='input-group-addon'>Tags</span><input name='" + settings.table_name + settings.row_count + "_free_tags' id='" + settings.table_name + settings.row_count + "_free_tags' type='text' class='form-control unstyled'></div>" +
-                    "<div id='" + settings.table_name + settings.row_count + "_free_tags_suggestions' style='padding-left:120px'></div>" +
+                    "<div class='input-group'><span class='input-group-addon'>Tags</span><input name='" +  e + settings.table_name + settings.row_count + "_free_tags' id='" +  e + settings.table_name + settings.row_count + "_free_tags' type='text' class='form-control unstyled'></div>" +
+                    "<div id='" +  e + settings.table_name + settings.row_count + "_free_tags_suggestions' style='padding-left:120px'></div>" +
                     "</div>" +
                     "</div>" +
                     "<div class='col-md-4'>" +
                     "<div class='form-group'>" +
-                    "<div class='input-group'><span class='input-group-addon'>Number of Voices</span><input name='" + settings.table_name + settings.row_count + "_number_of_voices' id='" + settings.table_name + settings.row_count + "_number_of_voices' type='number' class='form-control unstyled'></div>" +
+                    "<div class='input-group'><span class='input-group-addon'>Number of Voices</span><input name='" +  e + settings.table_name + settings.row_count + "_number_of_voices' id='" +  e + settings.table_name + settings.row_count + "_number_of_voices' type='number' class='form-control unstyled'></div>" +
                     "</div>" +
                     "</div>" +
                     "</div>" +
                     "<div class='row' style='padding-left:12%; padding-right:15px'>" +
                     "<div class='col-md-12'>" +
-                    "<strong>Comments:</strong> <div class='form-group'><textarea rows='7' class='form-control' name='" + settings.table_name + settings.row_count + "_comment' id='" + settings.table_name + settings.row_count + "_comment' style='resize:vertical'></textarea>" +
+                    "<strong>Comments:</strong> <div class='form-group'><textarea rows='7' class='form-control' name='" +  e + settings.table_name + settings.row_count + "_comment' id='" +  e + settings.table_name + settings.row_count + "_comment' style='resize:vertical'></textarea>" +
                     "</div>" +
                     "</div>" +
                     "</td></tr>");
@@ -96,32 +96,40 @@
             {
                 settings.t_size = settings.$table.children().length;
                 var append_row = "<tr id='" + e + settings.table_name + settings.row_count + "'><td class='text-center'><button id='del_" + settings.table_name + settings.row_count + "' type='button' tabindex='-1' class='btn btn-default'><span class='glyphicon glyphicon-remove'></span></button></td>";
-                if(exists){append_row +="<td><a name='" + settings.table_name + e + "_files_" + settings.row_count + "' id='" + settings.table_name + e + "_files_" + settings.row_count + "'></a></td>";}
+                if(exists){append_row +="<td><a name='" +  settings.table_name + e + "_files_" + settings.row_count + "' id='" + settings.table_name + e + "_files_" + settings.row_count + "'></a></td>";}
                 else{append_row += "<td><input name='" + settings.table_name + e + "_files_" + settings.row_count + "' id='" + settings.table_name + e + "_files_" + settings.row_count + "' type='file' multiple='multiple'></td>";}
                 settings.$table.append(append_row +
-                    "<td><select class='selectpicker show-tick' name='" + settings.table_name + e + "_parent_" + settings.row_count + "'id='" + settings.table_name + e + "_parent_" + settings.row_count + "'>" +
+                    "<td><select class='selectpicker show-tick' name='" + e + settings.table_name + "_parent_" + settings.row_count + "'id='" + e + settings.table_name + "_parent_" + settings.row_count + "'>" +
                     "<option value='piece'>Attach to Piece</option></select></td>" +
-                    "<td><input name='" + settings.table_name + e + "_source_" + settings.row_count + "' id='" + settings.table_name + e + "_source_" + settings.row_count + "' class='form-control' autocomplete='off' data-toggle='popover' data-placement='top' data-trigger='focus' data-html='true' title='<b>File Source</b>'" +
+                    "<td><input name='" +  e + settings.table_name + "_source_" + settings.row_count + "' id='" +  e + settings.table_name + "_source_" + settings.row_count + "' class='form-control' autocomplete='off' data-toggle='popover' data-placement='top' data-trigger='focus' data-html='true' title='<b>File Source</b>'" +
                     "data-content='Indicate the source of the file here, such as <em>Choral Wiki</em> or <em>Transcribed by Uploader</em>. If no source is provided, this file will be ignored. This file will be renamed automatically, so does not require a title.'> </td></tr>");
-                $("#" + settings.table_name + e + "_files_" + settings.row_count).filestyle({
+                $("#" +  e + settings.table_name + "_files_" + settings.row_count).filestyle({
                     buttonBefore: true,
                     iconName: "glyphicon-file",
                     buttonText: settings.file_button_name
                 });
             }
-            initRow(settings.row_count);
+            initRow(settings.row_count, exists);
             settings.row_count++;
         }
 
-        function initRow(row)
+        function initRow(row, exists)
         {
+            if (exists)
+            {
+                var e = "_existing"
+            }
+            else
+            {
+                var e = ""
+            }
             drawAttachSelects();
             $("#del_" + settings.table_name + row).on("click", function(event)
             {
                 var row_num = parseInt(event.currentTarget.getAttribute("id").substring(settings.table_name.length + 4));
                 $("#" + settings.table_name + "_files" + row_num).filestyle('destroy');
-                settings.$table.children().remove("#" + settings.table_name + row_num +  "_tags");
-                settings.$table.children().remove("#" + settings.table_name + row_num);
+                settings.$table.children().remove("#" + e + settings.table_name + row_num +  "_tags");
+                settings.$table.children().remove("#" + e + settings.table_name + row_num);
                 if (settings.table_name !== 'files')
                 {
                     renumberRows();
@@ -129,14 +137,14 @@
                 drawAttachSelects();
             });
 
-            $("#" + settings.table_name + "_title_" + row).on("keydown", function(event) {
+            $("#" + e + settings.table_name + "_title_" + row).on("keydown", function(event) {
                 if (event.keyCode === 13)
                 {
                     event.preventDefault();
                     return false
                 }
             });
-            $("#" + settings.table_name + "_title_" + row).focusout(function() {
+            $("#" + e + settings.table_name + "_title_" + row).focusout(function() {
                 drawAttachSelects();
             });
 
@@ -170,11 +178,10 @@
         }
 
         function drawAttachSelects(){
-            var movements = $("[id^=mov_title]");
+            var movements = $("[id^=mov_title], [id^=_existingmov_title] ");
             var content = "<option value='piece'>Attach to Piece</option>";
             var currentIDs = [];
-
-            var attachSelects = $("[id^=files_parent_],[id^=files_existing_parent_]");
+            var attachSelects = $("[id^=files_parent_],[id^=_existingfiles_parent_]");
             var i = 0;
             var oldVal = null;
             for (i = 0; i < movements.length; i++)
@@ -212,12 +219,17 @@
         {
             return addRow(exists);
         };
+        this.drawAttachSelects = function()
+        {
+            return drawAttachSelects();
+        };
     };
 
 
     var methods = {
         printStuff : function() {$(this).data('dynamicTable').printStuff();},
-        addRow : function(exists) {$(this).data('dynamicTable').addRow(exists);}
+        addRow : function(exists) {$(this).data('dynamicTable').addRow(exists);},
+        drawAttachSelects : function(){$(this).data('dynamicTable').drawAttachSelects()}
     };
 
 
