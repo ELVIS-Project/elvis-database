f527ec195efa76a6fbe0935919b4035f585b89a5
Alex Parmentier <a.g.parmentier@gmail.com>
2015-08-19 17:39:19 -0400

    Removed attachment view.
    It is never used.

--- elvis/elvis/urls.py
+++ elvis/elvis/urls.py
@@ -18,7 +18,6 @@ from elvis.views.movement import MovementList, MovementDetail
 from elvis.views.composer import ComposerList, ComposerDetail
 from elvis.views.collection import CollectionList, CollectionDetail, CollectionCurrent
 from elvis.views.tag import TagDetail
-from elvis.views.attachment import AttachmentList, AttachmentDetail
 from django.contrib.auth import views as auth_views
 
 # Uncomment the next two lines to enable the admin:
@@ -69,9 +68,6 @@ urlpatterns += format_suffix_patterns(
 
         url(r'^movements/$', MovementList.as_view(), name="movement-list"),
         url(r'^movement/(?P<pk>[0-9]+)/$', MovementDetail.as_view(), name="movement-detail"),
-
-        url(r'^attachments/$', AttachmentList.as_view(), name="attachment-list"),
-        url(r'^attachment/(?P<pk>[0-9]+)/$', AttachmentDetail.as_view(), name="attachment-detail"),
 
         url(r'^tag/(?P<pk>[0-9]+)/$', TagDetail.as_view(), name="tag-detail"),
 
