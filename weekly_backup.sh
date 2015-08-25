6c0dfb2094b696b5de3e8a481149befb0174b716
Alex Parmentier <a.g.parmentier@gmail.com>
2015-08-25 15:35:13 -0400

    Fixed: Sending much less over internet now.
    Since the hardlinks aren't going to work on this osx box, its better to copy the latest backup over before rsyncing.

--- weekly_backup.sh
+++ weekly_backup.sh
@@ -11,8 +11,8 @@ echo Dumping database to JSON.
 python manage.py dumpdata --natural-foreign --exclude contenttypes --exclude auth.permission > ../elvis-database-backups/backup.0/$(echo $DATE).json
 
 echo Preparing remote server...
-ssh AlexPar@132.206.14.118 'cd ~/ELVISDB_BACKUP/; rm -rf backup.3; mv backup.2 backup.3; mv backup.1 backup.2; mv backup.0 backup.1;'
+ssh AlexPar@132.206.14.118 'cd ~/ELVISDB_BACKUP/; rm -rf backup.3; mv backup.2 backup.3; mv backup.1 backup.2; cp -r backup.0 backup.1;'
 
 echo Synching to remote server...
-rsync -r --delete --link-dest=/users/AlexPar/ELVISDB_BACKUP/backup.1 /srv/webapps/elvis-database-backups/backup.0/ AlexPar@132.206.14.118:~/ELVISDB_BACKUP/backup.0/
+rsync -r --delete /srv/webapps/elvis-database-backups/backup.0/ AlexPar@132.206.14.118:~/ELVISDB_BACKUP/backup.0/
 echo Done! $Date weekly backup complete.
