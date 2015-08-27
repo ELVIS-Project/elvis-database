52066a86b1445c845a0b876393bb9efa1bcea1a3
Alex Parmentier <a.g.parmentier@gmail.com>
2015-08-27 15:22:33 -0400

    Fixed: Local scripts don't try to load a non-existing environment now.

--- scripts/start_celery.sh
+++ scripts/start_celery.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-source elvis_env/bin/activate
 cd /home/lexpar/Documents/DDMAL/elvis-database/elvis/
 celery -A elvis worker -l info
--- scripts/start_django.sh
+++ scripts/start_django.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-source elvis_env/bin/activate
 python manage.py runserver_plus
