dfbd68c671fad6f988a0cf2d6d37d98393e3468d
Alex Parmentier <a.g.parmentier@gmail.com>
2016-01-05 19:21:39 -0500

    WIP: Cart script sends lighter requests.
    The script keeps a second list in memory which only contains id and type and is used to query status of cart items. This is a significantly lighter query then sending over the full items list.

--- elvis/static/js/elvis-scripts/cartButtonScript.js
+++ elvis/static/js/elvis-scripts/cartButtonScript.js
@@ -3,6 +3,7 @@ var action = null;
 var $button = null;
 var cart_timeout = null;
 var items = null;
+var light_items = {};
 var $forms = null;
 
 function cartButtonRefresh()
@@ -16,44 +17,24 @@ function cartButtonRefresh()
 
         $button = $(this);
         $button.tooltip('hide');
+
         if ($button.hasClass('disabled'))
         {
             $button.blur();
             return false
         }
+
+        $button.addClass("disabled");
         if ($button.hasClass('cart-badge'))
             type = 'badge';
         else
             type = 'button';
+
         if ($button.hasClass('btn-success'))
             action = 'add';
         else
             action = 'remove';
 
-        if (action === 'add')
-        {
-            cart_timeout = setTimeout(function ()
-            {
-                $base_modal_header.html("<h4 class='modal-title'>Adding to Downloads...</h4>");
-                $base_modal_body.html("<p>Adding large collections or composer oeuvres to your downloads can take a little time...</p>");
-                $base_modal_footer.html("<div class='progress'>" +
-                    "<div class='progress-bar progress-bar-striped active' role='progressbar' style='width: 100%'></div> " +
-                    "</div>");
-                $base_modal.modal('show');
-            }, 1000);
-        }
-        else
-        {
-            cart_timeout = setTimeout(function ()
-            {
-                $base_modal_header.html("<h4 class='modal-title'>Removing from Downloads...</h4>");
-                $base_modal_body.html("<p>Removing large collections or composer oeuvres from your downloads can take a little time...</p>");
-                $base_modal_footer.html("<div class='progress'>" +
-                    "<div class='progress-bar progress-bar-striped active' role='progressbar' style='width: 100%'></div> " +
-                    "</div>");
-                $base_modal.modal('show');
-            }, 1000);
-        }
         $.ajax({
             type: "post",
             url: "/download-cart/",
@@ -67,13 +48,14 @@ function cartButtonRefresh()
                     $collection_count.text("(" + data.count + ")");
                 });
                 $collection_count.fadeIn(100);
-                init_cart_buttons()
+                init_cart_buttons();
             },
             error: function (data)
             {
                 $base_modal_header.html("<h4 class='modal-title'>Error modifying downloads!</h4>");
                 $base_modal_body.html("<p>Something went wrong and your cart was not modified!</p>");
                 $base_modal_footer.html("<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>");
+                $base_modal.modal('show');
             }
         });
     });
@@ -85,21 +67,28 @@ function init_cart_buttons()
         $forms = $(".recursive-patch-download-form");
     if (items === null)
         items = build_item_dict($forms);
-    debugger;
 
     $.ajax({
         url: "/download-cart/",
-        data: {check_in_cart: JSON.stringify(items)},
+        data: {check_in_cart: JSON.stringify(light_items)},
         success: function (data)
         {
-            var keys = Object.keys(data);
-            for (var i = 0; i < keys.length; i++)
+            if (data !== null)
             {
-                var key = keys[i];
-                if (items[key]['button_type'] == "button")
-                    draw_button(data[key], items[key]["$elem"]);
-                else
-                    draw_badge(data[key], items[key]["$elem"]);
+                var keys = Object.keys(data);
+                for (var i = 0; i < keys.length; i++)
+                {
+                    var key = keys[i];
+                    var back = data[key]['in_cart'];
+                    if (data[key]['in_cart'] === items[key]['in_cart'])
+                        continue;
+                    items[key]['in_cart'] = back;
+                    light_items[key]['in_cart'] = back;
+                    if (items[key]['button_type'] == "button")
+                        draw_button(data[key], items[key]["$elem"]);
+                    else
+                        draw_badge(data[key], items[key]["$elem"]);
+                }
             }
             cartButtonRefresh()
         }
@@ -111,32 +100,21 @@ function build_item_dict($forms)
     items = {};
     for (var i = 0; i < $forms.size(); i++)
     {
-        var form_children = $forms[i].children;
-        var item_type = null, item_id = null, button_type = null;
-        for (var j in form_children)
-        {
-            if(item_type !== null && item_id !== null && button_type !== null)
-                break;
-
-            var child = form_children[j];
-            if (child.name === "item_type")
-            {
-                item_type = child.value;
-                continue;
-            }
-            if (child.name === "id")
-            {
-                item_id = child.value;
-            }
-            if (child.name === "button_type")
-            {
-                button_type = child.value;
-            }
+        var fields = $forms[i].children;
+        items[fields[2].value] = {
+            "button_type": fields[0].value,
+            "type": fields[1].value,
+            "$elem": $($forms[i]),
+            "in_cart": "init"
+        };
+        light_items[fields[2].value] = {
+            "type": fields[1].value,
+            "in_cart": "init"
         }
-        items[item_id] = {"type": item_type, "button_type": button_type, "$elem": $($forms[i])};
     }
     return items
 }
+
 
 function draw_button(data, element)
 {
