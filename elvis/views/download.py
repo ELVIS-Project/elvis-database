54f357d2428bb36039461f520a66c66bec84060d
AFFogarty <andrew.f.fogarty@gmail.com>
2016-02-09 16:06:00 -0500

    New: Refactored download view code to pull out functions

--- elvis/views/download.py
+++ elvis/views/download.py
@@ -26,6 +26,182 @@ class DownloadCartHTMLRenderer(CustomHTMLRenderer):
     template_name = "download/download_cart.html"
 
 
+def _try_get(model, obj_id):
+    """ Try to get an object out of the database.
+
+    Returns None if the object no longer exists.
+    It is the caller's responsibility to handle this situation.
+
+    :param model: An elvis model to query on.
+    :param obj_id: The uuid of the object to find.
+    :return: The object or None.
+    """
+    tmp = None
+    try:
+        tmp = model.objects.get(uuid=obj_id)
+    except ObjectDoesNotExist:
+        pass
+    return tmp
+
+
+def _check_in_cart(cart, items):
+    """Create dict of differences between frontend/backend cart.
+
+    :param cart: The user's cart stored in request.session['cart'].
+    :param items: A dict of item's with the following format:
+    {[uuid]:{'type': 'elvis_[type]', 'in_cart': bool}
+    :return: A dict in the same format (minus 'type' key as it is
+    unnecessary) of only the pieces who's 'in_cart' status is different.
+    """
+
+    results = {}
+    for key in items.keys():
+        if items[key]['type'] == "elvis_composer":
+            back = cart.get("COM-" + key, False)
+        elif items[key]['type'] == "elvis_collection":
+            back = cart.get("COL-" + key, False)
+        elif items[key]['type'] == "elvis_piece":
+            back = cart.get("P-" + key, False)
+        elif items[key]['type'] == "elvis_movement":
+            mov = _try_get(Movement, key)
+            if mov and cart.get(mov.parent_cart_id, False):
+                back = "Piece"
+            else:
+                back = cart.get("M-" + key, False)
+        front = items[key]['in_cart']
+        if front == back:
+            continue
+        results[key] = {"in_cart": back}
+    return results
+
+
+def _retrieve_piece(pid, request, exts):
+    """Use cache to speed up retrieving user cart contents.
+
+    Warning: If the item no longer exists in the database, the uuid
+    will be removed from the user's cart and None returned.
+
+    :param pid: The id stored in the session for carts (P-[uuid])
+    :param request: The request object (for serialization)
+    :return: A serialized dict representing the Piece or None.
+    """
+    p_uuid = pid[2:]
+    p = cache.get("EMB-" + p_uuid)
+    if not p:
+        tmp = _try_get(Piece, p_uuid)
+        if not tmp:
+            del(request.session['cart'][pid])
+            return None
+        p = PieceEmbedSerializer(tmp, context={'request': request}).data
+
+    for a in p['attachments']:
+        exts[a['extension']] += 1
+        exts['total'] += 1
+
+    for m in p['movements']:
+        for a in m['attachments']:
+            exts[a['extension']] += 1
+            exts['total'] += 1
+
+    return p
+
+
+def _retrieve_movement(mid, request, exts):
+    """Use cache to speed up retrieving user cart contents.
+
+    Warning: If the item no longer exists in the database, the uuid
+    will be removed from the user's cart and None returned.
+
+    :param mid: The id stored in the session for carts (M-[uuid]).
+    :param request: The request object (for serialization).
+    :return: A serialized dict representing the Movement or None.
+    """
+    m_uuid = mid[2:]
+    m = cache.get("EMB-" + m_uuid)
+    if not m:
+        tmp = _try_get(Movement, m_uuid)
+        if not tmp:
+            del(request.session['cart'][mid])
+            return None
+        m = MovementEmbedSerializer(tmp, context={'request': request}).data
+
+    for a in m['attachments']:
+        exts[a['extension']] += 1
+        exts['total'] += 1
+
+    return m
+
+
+def remove_item(item_type, item_uuid, cart):
+    """
+    Remove item from a cart.
+
+    :param item_type:
+    :param item_uuid:
+    :param cart:
+    :return:
+    """
+    if item_type == "elvis_movement":
+        cart.pop("M-" + item_uuid, None)
+    elif item_type == "elvis_piece":
+        cart.pop("P-" + item_uuid, None)
+        piece = _try_get(Piece, item_uuid)
+        if piece:
+            for mov in piece.movements.all():
+                remove_item("elvis_movement", str(mov.uuid), cart)
+    elif item_type == "elvis_collection":
+        cart.pop("COL-" + item_uuid, None)
+        coll = Collection.objects.filter(uuid=item_uuid)[0]
+        for piece in coll.pieces.all():
+            cart.pop("P-" + str(piece.uuid), None)
+        for mov in coll.movements.all():
+            cart.pop("M-" + str(mov.uuid), None)
+    elif item_type == "elvis_composer":
+        cart.pop("COM-" + item_uuid, None)
+        comp = Composer.objects.filter(uuid=item_uuid)[0]
+        for piece in comp.pieces.all():
+            cart.pop("P-" + str(piece.uuid), None)
+        for mov in comp.movements.all():
+            cart.pop("M-" + str(mov.uuid), None)
+
+
+def add_item(item_type, item_uuid, cart):
+    """
+    Add item to a cart.
+
+    :param item_type:
+    :param item_uuid:
+    :param cart:
+    :return:
+    """
+    if item_type == "elvis_movement":
+        cart["M-" + item_uuid] = True
+    elif item_type == "elvis_piece":
+        cart["P-" + item_uuid] = True
+        piece = _try_get(Piece, item_uuid)
+        if piece:
+            for mov in piece.movements.all():
+                remove_item("elvis_movement", str(mov.uuid), cart)
+    elif item_type == "elvis_collection":
+        cart["COL-" + item_uuid] = True
+        coll = Collection.objects.filter(uuid=item_uuid)[0]
+        for piece in coll.pieces.all():
+            cart["P-" + str(piece.uuid)] = True
+        for mov in coll.movements.all():
+            parent = mov.piece
+            if not parent or not cart.get("P-" + str(parent.uuid)):
+                cart["M-" + str(mov.uuid)] = True
+    elif item_type == "elvis_composer":
+        cart["COM-" + item_uuid] = True
+        comp = Composer.objects.filter(uuid=item_uuid)[0]
+        for piece in comp.pieces.all():
+            cart["P-" + str(piece.uuid)] = True
+        for mov in comp.movements.all():
+            parent = mov.piece
+            if not parent or not cart.get("P-" + str(parent.uuid)):
+                cart["M-" + str(mov.uuid)] = True
+
+
 class DownloadCart(generics.GenericAPIView):
     permission_classes = (permissions.IsAuthenticated,)
     renderer_classes = (JSONRenderer, DownloadCartHTMLRenderer)
@@ -34,18 +210,19 @@ class DownloadCart(generics.GenericAPIView):
         data = self.make_cart_response(request)
         return Response(data, *args, **kwargs)
 
-    def make_cart_response(self, request):
+    @staticmethod
+    def make_cart_response(request):
         cart = request.session.get('cart', {})
         cart_copy = cart.copy()
         data = {"pieces": [], "movements": []}
         ext_count = defaultdict(int)
         for key in cart_copy.keys():
             if key.startswith("P"):
-                tmp = self._retrieve_piece(key, request, ext_count)
+                tmp = _retrieve_piece(key, request, ext_count)
                 if tmp:
                     data['pieces'].append(tmp)
             if key.startswith("M"):
-                tmp = self._retrieve_movement(key, request, ext_count)
+                tmp = _retrieve_movement(key, request, ext_count)
                 if tmp:
                     data['movements'].append(tmp)
 
@@ -53,109 +230,6 @@ class DownloadCart(generics.GenericAPIView):
         data['extension_counts'] = ext_list
         data['attachment_count'] = ext_count['total']
         return data
-
-    def _retrieve_piece(self, pid, request, exts):
-        """Use cache to speed up retrieving user cart contents.
-
-        Warning: If the item no longer exists in the database, the uuid
-        will be removed from the user's cart and None returned.
-
-        :param pid: The id stored in the session for carts (P-[uuid])
-        :param request: The request object (for serialization)
-        :return: A serialized dict representing the Piece or None.
-        """
-        p_uuid = pid[2:]
-        p = cache.get("EMB-" + p_uuid)
-        if not p:
-            tmp = self._try_get(Piece, p_uuid)
-            if not tmp:
-                del(request.session['cart'][pid])
-                return None
-            p = PieceEmbedSerializer(tmp, context={'request': request}).data
-
-        for a in p['attachments']:
-            exts[a['extension']] += 1
-            exts['total'] += 1
-
-        for m in p['movements']:
-            for a in m['attachments']:
-                exts[a['extension']] += 1
-                exts['total'] += 1
-
-        return p
-
-    def _retrieve_movement(self, mid, request, exts):
-        """Use cache to speed up retrieving user cart contents.
-
-        Warning: If the item no longer exists in the database, the uuid
-        will be removed from the user's cart and None returned.
-
-        :param mid: The id stored in the session for carts (M-[uuid]).
-        :param request: The request object (for serialization).
-        :return: A serialized dict representing the Movement or None.
-        """
-        m_uuid = mid[2:]
-        m = cache.get("EMB-" + m_uuid)
-        if not m:
-            tmp = self._try_get(Movement, m_uuid)
-            if not tmp:
-                del(request.session['cart'][mid])
-                return None
-            m = MovementEmbedSerializer(tmp, context={'request': request}).data
-
-        for a in m['attachments']:
-            exts[a['extension']] += 1
-            exts['total'] += 1
-
-        return m
-
-    def _try_get(self, model, obj_id):
-        """ Try to get an object out of the database.
-
-        Returns None if the object no longer exists.
-        It is the caller's responsibility to handle this situation.
-
-        :param model: An elvis model to query on.
-        :param obj_id: The uuid of the object to find.
-        :return: The object or None.
-        """
-        tmp = None
-        try:
-            tmp = model.objects.get(uuid=obj_id)
-        except ObjectDoesNotExist:
-            pass
-        return tmp
-
-    def _check_in_cart(self, cart, items):
-        """Create dict of differences between frontend/backend cart.
-
-        :param cart: The user's cart stored in request.session['cart'].
-        :param items: A dict of item's with the following format:
-        {[uuid]:{'type': 'elvis_[type]', 'in_cart': bool}
-        :return: A dict in the same format (minus 'type' key as it is
-        unnecessary) of only the pieces who's 'in_cart' status is different.
-        """
-
-        results = {}
-        for key in items.keys():
-            if items[key]['type'] == "elvis_composer":
-                back = cart.get("COM-" + key, False)
-            elif items[key]['type'] == "elvis_collection":
-                back = cart.get("COL-" + key, False)
-            elif items[key]['type'] == "elvis_piece":
-                back = cart.get("P-" + key, False)
-            elif items[key]['type'] == "elvis_movement":
-                mov = self._try_get(Movement, key)
-                if mov and cart.get(mov.parent_cart_id, False):
-                    back = "Piece"
-                else:
-                    back = cart.get("M-" + key, False)
-            front = items[key]['in_cart']
-            if front == back:
-                continue
-            results[key] = {"in_cart": back}
-        return results
-
 
     @method_decorator(csrf_protect)
     def post(self, request, *args, **kwargs):
@@ -173,13 +247,14 @@ class DownloadCart(generics.GenericAPIView):
         elif 'check_in_cart' in request.POST:
             cart = request.session.get('cart', {})
             items = json.loads(request.POST['check_in_cart'])
-            results = self._check_in_cart(cart, items)
+            results = _check_in_cart(cart, items)
             jresults = json.dumps(results)
             return HttpResponse(jresults, content_type="application/json")
         else:
             return self.update_cart(request)
 
-    def update_cart(self, request):
+    @staticmethod
+    def update_cart(request):
         cart = request.session.get('cart', {})
         items = request.POST.get('items', [])
         for item in items:
@@ -187,73 +262,21 @@ class DownloadCart(generics.GenericAPIView):
             item_uuid = item.get('id')
             action = item.get('action')
             if action == 'add':
-                self.add_item(item_type, item_uuid, cart)
+                add_item(item_type, item_uuid, cart)
             if action == 'remove':
-                self.remove_item(item_type, item_uuid, cart)
+                remove_item(item_type, item_uuid, cart)
 
         item_type = request.POST.get('item_type')
         item_uuid = request.POST.get('id')
         action = request.POST.get('action')
         if action == 'add':
-            self.add_item(item_type, item_uuid, cart)
+            add_item(item_type, item_uuid, cart)
         if action == 'remove':
-            self.remove_item(item_type, item_uuid, cart)
+            remove_item(item_type, item_uuid, cart)
 
         request.session['cart'] = cart
         jresults = json.dumps({'count': len(cart)})
         return HttpResponse(content=jresults, content_type="json")
-
-    def add_item(self, item_type, item_uuid, cart):
-        if item_type == "elvis_movement":
-            cart["M-" + item_uuid] = True
-        elif item_type == "elvis_piece":
-            cart["P-" + item_uuid] = True
-            piece = self._try_get(Piece, item_uuid)
-            if piece:
-                for mov in piece.movements.all():
-                    self.remove_item("elvis_movement", str(mov.uuid), cart)
-        elif item_type == "elvis_collection":
-            cart["COL-" + item_uuid] = True
-            coll = Collection.objects.filter(uuid=item_uuid)[0]
-            for piece in coll.pieces.all():
-                cart["P-" + str(piece.uuid)] = True
-            for mov in coll.movements.all():
-                parent = mov.piece
-                if not parent or not cart.get("P-" + str(parent.uuid)):
-                    cart["M-" + str(mov.uuid)] = True
-        elif item_type == "elvis_composer":
-            cart["COM-" + item_uuid] = True
-            comp = Composer.objects.filter(uuid=item_uuid)[0]
-            for piece in comp.pieces.all():
-                cart["P-" + str(piece.uuid)] = True
-            for mov in comp.movements.all():
-                parent = mov.piece
-                if not parent or not cart.get("P-" + str(parent.uuid)):
-                    cart["M-" + str(mov.uuid)] = True
-
-    def remove_item(self, item_type, item_uuid, cart):
-        if item_type == "elvis_movement":
-            cart.pop("M-" + item_uuid, None)
-        elif item_type == "elvis_piece":
-            cart.pop("P-" + item_uuid, None)
-            piece = self._try_get(Piece, item_uuid)
-            if piece:
-                for mov in piece.movements.all():
-                    self.remove_item("elvis_movement", str(mov.uuid), cart)
-        elif item_type == "elvis_collection":
-            cart.pop("COL-" + item_uuid, None)
-            coll = Collection.objects.filter(uuid=item_uuid)[0]
-            for piece in coll.pieces.all():
-                cart.pop("P-" + str(piece.uuid), None)
-            for mov in coll.movements.all():
-                cart.pop("M-" + str(mov.uuid), None)
-        elif item_type == "elvis_composer":
-            cart.pop("COM-" + item_uuid, None)
-            comp = Composer.objects.filter(uuid=item_uuid)[0]
-            for piece in comp.pieces.all():
-                cart.pop("P-" + str(piece.uuid), None)
-            for mov in comp.movements.all():
-                cart.pop("M-" + str(mov.uuid), None)
 
 
 class Downloading(generics.GenericAPIView):
@@ -280,7 +303,6 @@ class Downloading(generics.GenericAPIView):
             task_id = str(uuid.uuid4())
             tasks.zip_files.apply_async(args=[cart, extensions, request.user.username], task_id=task_id)
             return Response({"task": task_id}, status=status.HTTP_200_OK)
-
 
         return Response(status=status.HTTP_200_OK)
 
