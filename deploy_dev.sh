16fc75b59265f606716844de51ea0df4c66369c2
AFFogarty <andrew.f.fogarty@gmail.com>
2016-02-11 16:24:20 -0500

    Fixed: Virtualenv paths

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
+# Save a backup
+mv "../dev" "../dev_old_$(date +%s)"
+
 # Clone the repo
-git clone -b dev git@github.com:ELVIS-Project/elvis-database.git ../dev_deploy
+git clone -b dev git@github.com:ELVIS-Project/elvis-database.git ../dev
 
 # Set up the virtualenv
-virtualenv -p python3 ../dev_deploy/.env
+virtualenv -p python3 ../dev/.env
 
 # Install requirements
-source ../dev_deploy/.env/bin/activate
-pip install -r ../dev_deploy/requirements.txt
+source ../dev/.env/bin/activate
+pip install -r ../dev/requirements.txt
 
 # Perform Django management tasks
-python ../dev_deploy/manage.py collectstatic --noinput
-python ../dev_deploy/manage.py migrate --noinput
-
-# Switch the directory names
-mv "../dev" "../dev_old_$(date +%s)"
-mv "../dev_deploy" "../dev"
+python ../dev/manage.py collectstatic --noinput
+python ../dev/manage.py migrate --noinput
