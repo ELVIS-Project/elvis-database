cd7e6e943a384c6a7496461f83a531a2536a1ae1
AFFogarty <andrew.f.fogarty@gmail.com>
2016-02-11 15:24:20 -0500

    New: Latest version of celery script

---
+++ celery_start_dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Virtual env path
+VIRTUAL_ENV=/srv/webapps/elvisdb/dev/.env
+PROJECT_PATH=/srv/webapps/elvisdb/dev/.env/elvis
+# Activate
+source ${VIRTUAL_ENV}/bin/activate
+# Move to project directory
+cd ${PROJECT_PATH}
+# Run your worker... old: exec celery worker -A elvis -l DEBUG --loglevel=INFO
+exec celery worker -A elvis -l info --pidfile="/run/celery/%n-dev.pid"
