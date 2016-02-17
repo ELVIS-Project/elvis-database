2a9ba08f2aae848b35296a073722d1aa9a44986e
AFFogarty <andrew.f.fogarty@gmail.com>
2016-02-17 11:36:24 -0500

    Fixed: Absolute paths in deploy script

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
+BASE_DIR="/srv/webapps/elvisdb"
+
 # Save a backup
-mv "../dev" "../old/dev_$(date +%s)"
+mv "${BASE_DIR}/dev" "${BASE_DIR}/old/dev_$(date +%s)"
 
 # Clone the repo
-git clone -b dev git@github.com:ELVIS-Project/elvis-database.git ../dev
+git clone -b dev git@github.com:ELVIS-Project/elvis-database.git ${BASE_DIR}/dev
 
 # Set up the virtualenv
-virtualenv -p python3 ../dev/.env
+virtualenv -p python3 ${BASE_DIR}/dev/.env
 
 # Install requirements
-source ../dev/.env/bin/activate
-pip install -r ../dev/requirements.txt
+source ${BASE_DIR}/dev/.env/bin/activate
+pip install -r ${BASE_DIR}/dev/requirements.txt
 
 # Perform Django management tasks
-python ../dev/manage.py collectstatic --noinput
-python ../dev/manage.py migrate --noinput
+python ${BASE_DIR}/dev/manage.py collectstatic --noinput
+python ${BASE_DIR}/dev/manage.py migrate --noinput
 
 # Restart supervisor processes
 sudo supervisorctl restart elvis-db-dev
