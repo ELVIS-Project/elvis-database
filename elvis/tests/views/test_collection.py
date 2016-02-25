f365d20f9f6f3eee3d414db178556894c5542b2e
AFFogarty <andrew.f.fogarty@gmail.com>
2016-02-25 12:21:24 -0500

    New: Collection tests pass
    Refs #199

--- elvis/tests/views/test_collection.py
+++ elvis/tests/views/test_collection.py
@@ -42,37 +42,42 @@ class CollectionViewTestCase(ElvisTestSetup, APITestCase):
         response = self.client.get("/download-cart/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.client.logout()
-#
-#     def update_collection_not_allowed(self):
-#         collection = Collection.objects.filter(public=False)[0]
-#         response = self.client.post('/collections/', {'action': 'make-public', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#         response = self.client.post('/collections/', {'action': 'make-private', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#         response = self.client.post('/collections/', {'action': 'delete', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#
-#         self.client.login(username='testuser', password='test')
-#         response = self.client.post('/collections/', {'action': 'make-public', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-#         response = self.client.post('/collections/', {'action': 'make-private', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#         response = self.client.post('/collections/', {'action': 'delete', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#         self.client.logout()
-#
-#     def update_collection_allowed(self):
-#         collection = Collection.objects.filter(public=False)[0]
-#         self.client.login(username='creatoruser', password='test')
-#         response = self.client.post('/collections/', {'action': 'make-public', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
-#         response = self.client.post('/collections/', {'action': 'make-private', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
-#         response = self.client.post('/collections/', {'action': 'nonesense', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-#         response = self.client.post('/collections/', {'action': 'delete', 'id': collection.id})
-#         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
-#         response = self.client.post('/collections/', {'title': 'Test', 'comment': 'Test', 'creator': self.creator_user})
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.client.logout()
-#
+
+    def update_collection_not_allowed(self):
+        collection = Collection.objects.filter(public=False)[0]
+        response = self.client.post('/collections/', {'action': 'make-public',
+                                                      'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response = self.client.post('/collections/', {'action': 'make-private',
+                                                      'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response = self.client.post('/collections/', {'action': 'delete',
+                                                      'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.client.login(username='testuser', password='test')
+        response = self.client.post('/collections/', {'action': 'make-public',
+                                                      'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        response = self.client.post('/collections/', {'action': 'make-private',
+                                                      'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response = self.client.post('/collections/', {'action': 'delete',
+                                                      'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.client.logout()
+
+    def update_collection_allowed(self):
+        collection = Collection.objects.filter(public=False)[0]
+        self.client.login(username='creatoruser', password='test')
+        response = self.client.post('/collections/', {'action': 'make-public', 'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        response = self.client.post('/collections/', {'action': 'make-private', 'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        response = self.client.post('/collections/', {'action': 'nonesense', 'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        response = self.client.post('/collections/', {'action': 'delete', 'id': collection.id})
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        response = self.client.post('/collections/', {'title': 'Test', 'comment': 'Test', 'creator': self.creator_user})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.client.logout()
