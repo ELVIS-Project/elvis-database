b67ec570571cbe4df078ff778bcc7d702aa39e42
AFFogarty <andrew.f.fogarty@gmail.com>
2016-02-25 12:32:56 -0500

    Fixed: Composer tests pass
    Refs #199

--- elvis/tests/views/test_composer.py
+++ elvis/tests/views/test_composer.py
@@ -1,21 +1,28 @@
-# from rest_framework.test import APITestCase
-# from rest_framework import status
-# from elvis.tests.helpers import ElvisTestSetup
-# from elvis.models.composer import Composer
-#
-#
-# class ComposerViewTestCase(ElvisTestSetup, APITestCase):
-#     def setUp(self):
-#         self.setUp_elvis()
-#         self.setUp_user()
-#         self.setUp_test_models()
-#
-#     def test_get_list(self):
-#         response = self.client.get("/composers/")
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_get_detail(self):
-#         composer = Composer.objects.first()
-#         response = self.client.get("/composer/{0}/".format(composer.id))
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(response.data['id'], composer.id)
+from rest_framework.test import APITestCase
+from rest_framework import status
+from elvis.tests.helpers import ElvisTestSetup
+from elvis.models.composer import Composer
+
+
+class ComposerViewTestCase(ElvisTestSetup, APITestCase):
+    def setUp(self):
+        self.setUp_elvis()
+        self.setUp_user()
+        self.setUp_test_models()
+
+    def test_get_list(self):
+        """
+        Query the API for the list of composers.  Check that it returns 200.
+        """
+        response = self.client.get("/composers/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail(self):
+        """
+        Query the API for the details of a composer.  Check that API returns the
+        correct UUID.
+        """
+        composer = Composer.objects.first()
+        response = self.client.get("/composer/{0}/".format(composer.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['uuid'], str(composer.uuid))
