3b56322181d5c530625d30bb060f778a5904813b
AFFogarty <andrew.f.fogarty@gmail.com>
2016-02-25 12:38:12 -0500

    Fixed: Movement tests pass
    Refs #199

--- elvis/tests/views/test_movement.py
+++ elvis/tests/views/test_movement.py
@@ -1,21 +1,21 @@
-# from rest_framework.test import APITestCase
-# from rest_framework import status
-# from elvis.tests.helpers import ElvisTestSetup
-# from elvis.models.movement import Movement
-#
-#
-# class MovementViewTestCase(ElvisTestSetup, APITestCase):
-#     def setUp(self):
-#         self.setUp_elvis()
-#         self.setUp_user()
-#         self.setUp_test_models()
-#
-#     def test_get_list(self):
-#         response = self.client.get("/movements/")
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_get_detail(self):
-#         movement = Movement.objects.first()
-#         response = self.client.get("/movement/{0}/".format(movement.id))
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(response.data['uuid'], str(movement.uuid))
+from rest_framework.test import APITestCase
+from rest_framework import status
+from elvis.tests.helpers import ElvisTestSetup
+from elvis.models.movement import Movement
+
+
+class MovementViewTestCase(ElvisTestSetup, APITestCase):
+    def setUp(self):
+        self.setUp_elvis()
+        self.setUp_user()
+        self.setUp_test_models()
+
+    def test_get_list(self):
+        response = self.client.get("/movements/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail(self):
+        movement = Movement.objects.first()
+        response = self.client.get("/movement/{0}/".format(movement.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['uuid'], str(movement.uuid))
