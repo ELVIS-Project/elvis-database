9252f17bf1c737fa683fdae18ac2c15dfaa711b5
AFFogarty <andrew.f.fogarty@gmail.com>
2016-02-25 14:18:07 -0500

    Fixed: Don't include test files in coverage
    Refs #199

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Don't include the virtualenv or the migrations code in coverage
-coverage run --omit=.env/*,elvis/migrations/*  manage.py test
+coverage run --omit=.env/*,elvis/migrations/*,elvis/tests/*  manage.py test
 coverage html
 coverage report
