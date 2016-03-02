8d93b4eaa112e2a47e1aaf0274e97fdadbcaaf21
AFFogarty <andrew.f.fogarty@gmail.com>
2016-03-02 14:26:58 -0500

    New: Can remove collection movements

--- elvis/static/js/template-scripts/collection-detail.js
+++ elvis/static/js/template-scripts/collection-detail.js
@@ -16,7 +16,7 @@ $(document).ready(function ($)
     {
         $.ajax({
             type: "patch",
-            url: "/collection/{{ content.id }}",
+            url: window.location.href,
             data: {'public': false},
             success: function (data)
             {
@@ -29,7 +29,7 @@ $(document).ready(function ($)
     {
         $.ajax({
             type: "patch",
-            url: "/collection/{{ content.id }}",
+            url: window.location.href,
             data: {'public': true},
             success: function (data)
             {
@@ -50,11 +50,11 @@ $(document).ready(function ($)
         {
             $.ajax({
                 type: "delete",
-                url: "/collection/{{ content.id }}",
+                url: window.location.href,
                 success: function (data)
                 {
                     console.log("Deleted {{ content.title }}");
-                    document.location.href = '/collections/?creator={{ user.username }}';
+                    document.location.href = '/collections/mine/';
                 }
             })
         })
@@ -64,13 +64,18 @@ $(document).ready(function ($)
         event.preventDefault();
 
         var pieces = [];
+        var movements = [];
         var names = [];
-        var pieceSelectors = $('input[name="remove-piece"]:checked').map(function() {
+        $('input[name="remove-piece"]:checked').map(function() {
             pieces.push(parseInt($(this).val()));
             names.push($(this)["context"]["parentElement"].textContent.trim());
         });
+        $('input[name="remove-movement"]:checked').map(function() {
+            movements.push(parseInt($(this).val()));
+            names.push($(this)["context"]["parentElement"].textContent.trim());
+        });
 
-        if (pieces.length > 0) {
+        if (names.length > 0) {
             $base_modal_header.html("<h4 class='modal-title'>Confirm Removal.</h4>");
             $base_modal_body.html(
                 "<p>Are you sure you want to remove the following items from the collection?</p>"
@@ -84,24 +89,25 @@ $(document).ready(function ($)
             {
                 $.ajax({
                     type: "delete",
-                    url: "/collection/{{ content.id }}/elements/",
+                    url: window.location.href + "elements/",
                     data: JSON.stringify({
-                        piece_ids: pieces
+                        piece_ids: pieces,
+                        movement_ids: movements
                     }),
                     "contentType": "application/json",
                     success: function (data) {
                         console.log("Success:", data);
-                        document.location.href = '/collection/{{ content.id }}/';
+                        document.location.href = window.location.href;
                     },
                     error: function(data) {
                         console.log("Error:", data);
-                        document.location.href = '/collection/{{ content.id }}/';
+                        document.location.href = window.location.href;
                     }
                 })
             });
         } else {
             $base_modal_header.html("<h4 class='modal-title'>Cannot Remove</h4>");
-            $base_modal_body.html("<p>Please select at least 1 piece to remove.</p>");
+            $base_modal_body.html("<p>Please select at least 1 piece or movement to remove.</p>");
             $base_modal_footer.html('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>');
             $base_modal.modal('show');
         }
