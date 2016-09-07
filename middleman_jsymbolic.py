f07868481794fc5f65b979a22a41a40dc4e9615d
vee-L <veronique.lagace2@mail.mcgill.ca>
2016-09-07 15:39:52 -0400

    formatting, adapted for mei and midi, some cleanup, docstrings

--- middleman_jsymbolic.py
+++ middleman_jsymbolic.py
@@ -8,32 +8,39 @@ import pickle
 #note: the workflow to be used needs to be loaded in rodan manually before starting to use the following script
 def get_rodan_token(username, password):
     token = requests.post('https://rodan.simssa.ca/auth/token/', data={'username': username, 'password': password})
-    return token
+    return json.loads(token.text)['token']
 
-
+"""
+Gets the urls of midi and mei files currently in the elvis database.
+"""
 def get_from_elvis(username, password):
-    midi_files_urls = []
-    # resp = requests.get('http://dev.database.elvisproject.ca/search/?filefilt=.midi&filefilt=.midi', auth = (username, password))
-    # pages = json.loads(resp.text)['paginator']['total_pages']
-    for page in range(1,39):
+    midimei_files_urls = []
+    resp = requests.get('http://dev.database.elvisproject.ca/search/?filefilt=.midi&filefilt=.mei', auth = (username, password))
+    pages = json.loads(resp.text)['paginator']['total_pages']
+    for page in range(1,pages+1):
         print(page)
-        search = requests.get('http://dev.database.elvisproject.ca/search/?filefilt[]=.midi&filefilt[]=.capx&page='+str(page), auth = (username, password))
+        search = requests.get('http://dev.database.elvisproject.ca/search/?filefilt[]=.midi&filefilt[]=.mei&page='+str(page), auth = (username, password))
         for object in json.loads(search.text)['object_list']:
             if 'pieces_searchable' in object.keys():
                 piece = requests.get('http://dev.database.elvisproject.ca/piece/'+str(object['id'])+'?format=json', auth=(username, password))
                 for attachment in json.loads(piece.text)['attachments']:
-                    if attachment['extension']==".midi":
-                        midi_files_urls.append(attachment['url'])
+                    if attachment['extension'] == ".midi" or attachment['extension'] == ".mei":
+                        midimei_files_urls.append(attachment['url'])
             else:
                 movement = requests.get('http://dev.database.elvisproject.ca/movement/'+str(object['id'])+'?format=json', auth=(username, password))
                 for attachment in json.loads(movement.text)['attachments']:
-                    if attachment['extension']==".midi":
-                        midi_files_urls.append(attachment['url'])
+                    if attachment['extension'] == ".midi" or attachment['extension'] == ".mei":
+                        midimei_files_urls.append([attachment['url'], attachment['extension']])
     with open('midi_urls', 'wb') as out_file:
-            pickle.dump(midi_files_urls, out_file)
+            pickle.dump(midimei_files_urls, out_file)
+    return midimei_files_urls
 
-
-def push_to_elvis(token):
+"""
+Pushes the resulting feature files from the jsymbolic run into the appropriate places in the database
+(entry corresponding to where the original file came from).
+TODO: figure out what is preventing uploads to what should be the proper media file location on the backend.
+"""
+def push_to_elvis(results, token):
     results = pickle.load(open('rodan_results_urls', 'rb'))
 
     for page in results:
@@ -58,35 +65,39 @@ def push_to_elvis(token):
 
     return resp
 
-def get_from_rodan(resource_id):
-    result = requests.get('https://rodan.simssa.ca/resources/?result_of_workflow_run='+str(resource_id), headers={})
-    result_files = json.loads(result.text)['results']
-    actual_files = []
-    for results in result_files:
-        file_location = results['resource_file']
-        print(file_location)
-        actual_files.append(requests.get(file_location, headers={}))
-    return actual_files
 
-def push_to_rodan(filename, file, project_url, resourcetype, token):
-    files={'files': (filename, file)}
-    print(filename)
-    resource_data={'project': project_url, 'type': resourcetype}
-    resource_upload = requests.post('https://rodan.simssa.ca/resources/', files=files, data=resource_data, headers={'Authorization': "Token "+token})
-    return resource_upload
+"""
+Download the files from the provided url list  and upload them into the appropriate rodan project as resources.
 
+"""
+def push_to_rodan(elvis_username, elvis_password, file_url_array, project_url, resourcetype, token):
+    resources = []
+    for url in file_url_array:
+        files={'files': ('@'.join(url[0].split('/')[3:])+url[1],
+                         requests.get('http://dev.database.elvisproject.ca'+url[0], auth=(elvis_username, elvis_password)))}
+        resource_data={'project': project_url, 'type': resourcetype}
+        resource_upload = requests.post('https://rodan.simssa.ca/resources/', files=files, data=resource_data, headers={'Authorization': "Token "+token})
+        resources.append(json.loads(resource_upload.text['url']))
+    return resources
+
+
+
+"""
+Load up the appropriate resources into the jsymbolic workflow, then run it, logging what goes wrong if it does.
+TODO: re-create a proper logging mechanism
+"""
 def run_workflow(token, workflow_url, input_url):
-    resourcelist = []
-    for pageno in range(1,json.loads(requests.get('https://rodan.simssa.ca/',
-                                                headers={'Authorization': "Token "+token}).text)['number_of_pages']+1):
-        resources = requests.get(''.format(str(pageno)),
-                                 headers={'Authorization': "Token "+token})
-        for i in json.loads(resources.text)['results']:
-            print(i)
-            resourcelist.append(i['url'])
+    # resourcelist = []
+    # for pageno in range(1,json.loads(requests.get('https://rodan.simssa.ca/',
+    #                                               headers={'Authorization': "Token "+token}).text)['number_of_pages']+1):
+    #     resources = requests.get(''.format(str(pageno)),
+    #                              headers={'Authorization': "Token "+token})
+    #     for i in json.loads(resources.text)['results']:
+    #         print(i)
+    #         resourcelist.append(i['url'])
         workflow_data = {"created": "null", "updated": "null", "workflow": workflow_url, "resource_assignments":
             {input_url: resourcelist},
-            "name": "jsymbolic_elvis", "description": "Run of Workflow jsymbolic_elvis"}
+                         "name": "jsymbolic_elvis", "description": "Run of Workflow jsymbolic_elvis"}
         workflow_run = requests.post('https://rodan.simssa.ca/workflowruns/', data=json.dumps(workflow_data), headers={'Content-Type': 'application/json','Authorization': "Token "+token})
 
     json.loads(workflow_run.text)['url']
@@ -100,16 +111,34 @@ def run_workflow(token, workflow_url, input_url):
         pickle.dump(all_results, out_file)
 
 
+"""
+Get the results from the jsymbolic workflow run  of all the previously uploaded files
+"""
+def get_from_rodan(resource_id):
+    result = requests.get('https://rodan.simssa.ca/resources/?result_of_workflow_run='+str(resource_id), headers={})
+    result_files = json.loads(result.text)['results']
+    actual_files = []
+    for results in result_files:
+        file_location = results['resource_file']
+        print(file_location)
+        actual_files.append(requests.get(file_location, headers={}))
+    return actual_files
+
+
+
+
+
+
 
 if __name__ == "__main__":
 
-    #get_from_elvis('','')
-    # midi_urls = pickle.load(open('midi_urls','rb'))
-    # for url in midi_urls:
-    #     elvis = requests.get('http://dev.database.elvisproject.ca'+str(url), auth=('', ''))
-    #     result = push_to_rodan(url.split("/", 3)[3].replace('/', '@'), elvis.content, '', '')
-    #     print(result.content)
-    #print(get_rodan_token('', '').content)
+#note: dummy usernames and password.
+#TODO: "make usernames, passwords, project urls and workflow urls command-line enterable"
+    current_token = get_rodan_token('rodan_username', 'rodan_password')
+    midi_and_mei_urls = get_from_elvis('elvis_username', 'elvis_password')
+
+
+
 
 
 
