77acc90e6971b97dd002bd2d176f2c7bd71fe63f
Alex Parmentier <a.g.parmentier@gmail.com>
2016-11-02 13:12:35 -0400

    Fixed: Made 'development' default setting type.

--- elvis/settings.py
+++ elvis/settings.py
@@ -39,7 +39,7 @@ DEVELOPMENT = 1
 LOCAL = 2
 
 # Specify which of the above setting types you wish to use here.
-SETTING_TYPE = LOCAL
+SETTING_TYPE = DEVELOPMENT
 assert SETTING_TYPE in [PRODUCTION, DEVELOPMENT, LOCAL], "Must choose a legal setting type."
 
 if SETTING_TYPE is not PRODUCTION:
