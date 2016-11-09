1dd8b5466f039d7507a35522f1a94dce252a78d7
Alex Parmentier <a.g.parmentier@gmail.com>
2016-11-09 11:28:36 -0500

    Fixed: Test user generation.
    User generation for tests now needs to set the TOS feature manually unless
    all tests will rely on UserForm working right.

--- elvis/tests/helpers.py
+++ elvis/tests/helpers.py
@@ -1,22 +1,35 @@
 from django.contrib.auth.models import User
 from django.test import Client
-from model_mommy import mommy
 from django.test import override_settings
+from model_mommy import mommy
 from rest_framework.test import APITestCase
 
+from elvis.models import UserProfile
 # Some user accounts to use for testing
-real_user = {
-    'username': 'testuser',
-    'password': 'test'
-}
-fake_user = {
-    'username': 'fake',
-    'password': 'fake'
-}
-creator_user = {
-    'username': 'creatoruser',
-    'password': 'test'
-}
+test_users = [
+    {
+        'username': 'test_user',
+        'accepted_tos': True
+    },
+    {
+        'username': 'creator_user',
+        'accepted_tos': True
+    },
+    {
+        'username': 'super_user',
+        'email': 'a@b.com',
+        'accepted_tos': True,
+        'is_superuser': True
+    },
+    {
+        'username': 'no_tos_user',
+    }
+]
+
+real_user = test_users[0]
+fake_user = {'username': 'fake', 'password': 'fake'}
+creator_user = test_users[1]
+
 
 @override_settings(SOLR_SERVER="http://localhost:8983/solr/elvisdb_test")
 class ElvisTestSetup(APITestCase):
@@ -40,6 +53,25 @@ class ElvisTestSetup(APITestCase):
         self.test_private_collection = mommy.make('elvis.Collection', public=False, creator=self.creator_user)
 
     def setUp_user(self):
-        self.test_user = User.objects.create_user(username=real_user['username'], password=real_user['password'])
-        self.creator_user = User.objects.create_user(username=creator_user['username'], password=creator_user['password'])
-        self.super_user = User.objects.create_superuser(username='superuser', email='a@g.com', password='test')
+        """Set up users for the test suite to use.
+
+        Here we explicitly create UserProfiles for the users and set their
+        accepted_tos values manually in order to avoid relying on the correct
+        functioning of the UserForm.
+        """
+
+        for user_info in test_users:
+            if user_info.get('is_superuser'):
+                user = User.objects.create_superuser(username=user_info['username'],
+                                                     email=user_info['email'],
+                                                     password='test')
+            else:
+                user = User.objects.create_user(username=user_info['username'],
+                                                password='test')
+            user_profile = UserProfile(user=user)
+            if user_info.get('accepted_tos'):
+                user_profile.accepted_tos = True
+            user_profile.save()
+
+            # Assign each user to an attribute name based on their username.
+            setattr(self, user_info['username'], user)
