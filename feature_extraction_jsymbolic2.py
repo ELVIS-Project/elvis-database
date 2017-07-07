89ce0cbc836a04faae45563e2fbd745c831ebf7c
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2017-07-07 18:36:55 -0400

    Fix #242, now the script can create 2 log files, one recording the content of stdout, one recoding the content of stderr

--- feature_extraction_jsymbolic2.py
+++ feature_extraction_jsymbolic2.py
@@ -3,6 +3,7 @@ from music21 import *
 import sys
 import subprocess
 from log import logger
+from contextlib import redirect_stderr
 
 
 def convert_xml_into_midi():
@@ -21,14 +22,15 @@ def convert_xml_into_midi():
 
 
 def extract_features():
-    #sys.stdout = open('./downloaded_files_extracted_features/extract_features_log.txt', 'w')
-    for id, fn in enumerate(os.listdir('./downloaded_files/')):
-        if(fn[-3:] == 'xml'):
-            continue  # skip xml at this point
-        else:
-            os.system('java -Xmx3072m -jar /Users/yaolongju/Downloads/jMIR_3_0_developer/jSymbolic2/dist/jSymbolic2.jar -configrun jSymbolicDefaultConfigs.txt ./downloaded_files/' + fn +
-                      ' ./downloaded_files_extracted_features/' + fn + '_feature_values.xml'
-                      ' ./downloaded_files_extracted_features/' + fn + '_feature_descriptions.xml' )
+    #sys.stderr = open('./downloaded_files_extracted_features/extract_features_log.txt', 'w')
+    #with open('./downloaded_files_extracted_features/extract_features_log.txt', 'w') as sys.stderr, redirect_stderr(sys.stderr):
+        for id, fn in enumerate(os.listdir('./downloaded_files/')):
+            if(fn[-3:] == 'xml'):
+                continue  # skip xml at this point
+            else:
+                os.system('java -Xmx3072m -jar /Users/yaolongju/Downloads/jMIR_3_0_developer/jSymbolic2/dist/jSymbolic2.jar -configrun jSymbolicDefaultConfigs.txt ./downloaded_files/' + fn +
+                          ' ./downloaded_files_extracted_features/' + fn + '_feature_values.xml'
+                          ' ./downloaded_files_extracted_features/' + fn + '_feature_descriptions.xml >> ./downloaded_files_extracted_features/extract_features_log.txt 2>> ./downloaded_files_extracted_features/extract_features_error_log.txt' )
 
 
 if __name__ == "__main__":
