c4a84955661ec09a7399365e114090316cc0de85
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2017-07-13 17:17:01 -0400

    Fix #248.

---
+++ correcting_MEI_files.py
@@ -0,0 +1,70 @@
+import os, sys
+sys.path.append('/Users/yaolongju/Documents/Projects/libmei/python')
+import pymei
+
+
+def modifying_MEI():
+    """
+    Remove 'breve' attribute which is not supported by jsymbolic2
+    :return:
+    """
+    for id, fn in enumerate(os.listdir('./downloaded_files/')):
+        if (fn[-3:] == 'mei' and fn.find('NEW') == -1):
+            print(fn)
+            doc = pymei.documentFromFile('./downloaded_files/' + fn).getMeiDocument()
+            notes = doc.getElementsByName('note')
+            for i, note in enumerate(notes):
+                dur = note.getAttribute('dur')
+                if(dur.getValue() == 'breve'):  # copy the note, change the duration into whole and tie them together
+                    layer = note.getAncestor('layer')
+                    note.addAttribute('dur', 'whole')
+                    newnote = pymei.MeiElement('note')
+                    newnote.addAttribute('pname', note.getAttribute('pname').getValue())
+                    newnote.addAttribute('oct', note.getAttribute('oct').getValue())
+                    newnote.addAttribute('dur', 'whole')
+                    dots = note.getAttribute('dots')
+                    if(dots != None):
+                        newnote.addAttribute('dots', dots.getValue())
+                    #if(i + 1 < len(notes)):
+                        #layer.addChildBefore(notes[i + 1], newnote)
+                    #else:
+                    layer.addChild(newnote)
+                    id1 = note.getId()
+                    newnote.setId(id1 + 'copy')
+                    id2 = newnote.getId()
+                    measure = note.getAncestor('measure')
+                    tie = pymei.MeiElement('tie')
+                    tie.addAttribute('startid', id1)
+                    tie.addAttribute('endid', id2)
+                    measure.addChild(tie)
+            rests = doc.getElementsByName("rest")
+            for i, rest in enumerate(rests):  # rest needs to fix as well
+                dur = rest.getAttribute('dur')
+                if (dur != None):
+                    if (dur.getValue() == 'breve'):  # copy the note, change the duration into whole and tie them together
+                        layer = rest.getAncestor('layer')
+                        rest.addAttribute('dur', 'whole')
+                        newrest = pymei.MeiElement('rest')
+                        newrest.addAttribute('dur', 'whole')
+                        dots = rest.getAttribute('dots')
+                        if (dots != None):
+                            newrest.addAttribute('dots', dots.getValue())
+                            # if(i + 1 < len(notes)):
+                            # layer.addChildBefore(notes[i + 1], newnote)
+                        # else:
+                        layer.addChild(newrest)
+                        id1 = rest.getId()
+                        newrest.setId(id1 + 'copy')
+                        id2 = newrest.getId()
+                        measure = rest.getAncestor('measure')
+                        tie = pymei.MeiElement('tie')
+                        tie.addAttribute('startid', id1)
+                        tie.addAttribute('endid', id2)
+                        measure.addChild(tie)
+            pymei.documentToFile(doc, './downloaded_files/' + fn[:-4] + 'NEW' + '.mei')
+
+
+
+if __name__ == "__main__":
+    modifying_MEI()
+
