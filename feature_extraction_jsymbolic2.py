1d1abddbc2efd073b5fbce9be8b56cf00c668992
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2017-08-01 15:45:24 -0400

    Validate all the files on https://github.com/DDMAL/mei-test-set. Most of them are valid, and a few of them have invalid ID.

--- feature_extraction_jsymbolic2.py
+++ feature_extraction_jsymbolic2.py
@@ -5,6 +5,25 @@ from correcting_MEI_files import modifying_MEI
 from validating_MEI_files import validation
 
 
+def convert_MEI_into_midi():
+    """
+    Convert the corrected MEI files into midi, so that jsymbolic2 can process all MEI files and extract features
+    :return:
+    """
+    if os.path.exists('./downloaded_files/MEI/NEW/MIDI/') is False:
+        os.mkdir('./downloaded_files/MEI/NEW/MIDI/')
+    flog = open('./downloaded_files/MEI/NEW/MIDI/MEI_to_midi_log.txt', 'w')
+    for id, fn in enumerate(os.listdir('./downloaded_files/MEI/NEW/')):
+        if (fn[-3:] != 'mei'):
+            continue  # only convert xml into midi
+        print(fn)
+        try:
+            s = converter.parse('./downloaded_files/MEI/NEW/' + fn)
+            s.write('midi', fp = './downloaded_files/MEI/NEW/MIDI/' + fn + '.midi')
+        #s.write('xml', fp='./downloaded_files/MEI/NEW/MIDI/' + fn + '.xml')
+        except:
+            print(fn, file=flog)
+            print(sys.exc_info()[0], file=flog)
 def convert_xml_into_midi():
     if os.path.exists('./downloaded_files/XML/MIDI/') is False:
         os.mkdir('./downloaded_files/XML/MIDI/')
@@ -40,16 +59,19 @@ def extract_features_per_folder(filepath, featurepath, path):
 
 
 def extract_features(path):
-    extract_features_per_folder('./downloaded_files/', './downloaded_files/extracted_features/', path)
+    #extract_features_per_folder('./downloaded_files/', './downloaded_files/extracted_features/', path)
     extract_features_per_folder('./downloaded_files/MEI/NEW/', './downloaded_files/MEI/NEW/extracted_features/', path)
-    extract_features_per_folder('./downloaded_files/XML/MIDI/', './downloaded_files/XML/MIDI/extracted_features/', path)
+    #extract_features_per_folder('./downloaded_files/XML/MIDI/', './downloaded_files/XML/MIDI/extracted_features/', path)
+    extract_features_per_folder('./downloaded_files/MEI/NEW/MIDI/', './downloaded_files/MEI/NEW/MIDI/extracted_features/',
+                                path)
 
 
 if __name__ == "__main__":
     #jsymbolic_path = input('please specify jsymbolic path')
-    jsymbolic_path = '/Users/yaolongju/Downloads/jMIR_3_0_developer/jSymbolic2/dist/jSymbolic2.jar'
-    rng_path = '/Users/yaolongju/Documents/Parser_for_medieval_chants/MEI/mei-all.rng.xml'
-    convert_xml_into_midi()
-    validation(rng_path)
+    jsymbolic_path = './jMIR_3_0_developer/jSymbolic2/dist/jSymbolic2.jar'
+    rng_path = './MEI_schemata_files/mei-all.rng'
+    #convert_xml_into_midi()
+    #validation(rng_path)
     modifying_MEI()
+    convert_MEI_into_midi()
     extract_features(jsymbolic_path)
--- validating_MEI_files.py
+++ validating_MEI_files.py
@@ -1,19 +1,20 @@
 import os
-
-def validation(rngpath):
-    if os.path.exists('./downloaded_files/MEI/') is False:
-        os.mkdir('./downloaded_files/MEI/')
-    for id, fn in enumerate(os.listdir('./downloaded_files/MEI/')):
-        print(fn)
-        print('xmllint --noout --relaxng ' + rngpath +
-                       ' ./downloaded_files/MEI/' + fn +
-              '>> ./downloaded_files/MEI/validation_log.txt 2>> ./downloaded_files/MEI/validation_error_log.txt')
-        if (fn[-3:] == 'mei'):
-            os.system('xmllint --noout --relaxng ' + rngpath
-                      + ' ./downloaded_files/MEI/' + fn + '>> ./downloaded_files/MEI/validation_log.txt 2>> ./downloaded_files/MEI/validation_error_log.txt')
-
+import os.path
+def validation(rngpath, meipath):
+    for dirpath, dirnames, filenames in os.walk(meipath):
+        for filename in [f for f in filenames if f.endswith(".mei")]:  # find all MEI files in the current folder and subfolders
+            print
+            os.path.join(dirpath, filename)
+            print('xmllint --noout --relaxng ' + rngpath + ' ' +
+                  os.path.join(dirpath, filename) +
+                  '>> ./downloaded_files/MEI/validation_log.txt 2>> ./downloaded_files/MEI/validation_error_log.txt')
+            os.system('xmllint --noout --relaxng ' + rngpath + ' ' +
+                  os.path.join(dirpath, filename) +
+                  '>> ./downloaded_files/MEI/validation_log.txt 2>> ./downloaded_files/MEI/validation_error_log.txt')
 
 if __name__ == "__main__":
     #rng_path = input('please input the schema file you want to validate the MEI files')
-    rng_path = '/Users/yaolongju/Documents/Projects/mei-all.rng'
-    validation(rng_path)
+    rng_path = './MEI_schemata_files/mei-all.rng'
+    #mei_path = './downloaded_files/MEI/'
+    mei_path = '/Users/yaolongju/Documents/Projects/mei-test-set/MEI-test-set'
+    validation(rng_path, mei_path)
